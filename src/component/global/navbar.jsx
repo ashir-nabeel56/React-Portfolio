151e032a51589b0c543fd0f1ba46ae2a51544bdd
Nabeel <nabeelajmeri0@gmail.com>
2026-09-15 18:29:57 +0500

    some thing change

--- src/component/global/navbar.jsx
+++ src/component/global/navbar.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import './all.css'
-import { Link, NavLink } from 'react-router-dom'
+import {  NavLink } from 'react-router-dom'
 import logo from '../../assist/logo.png'
 
 function Navbar() {
